0326fdb6b55b4051edd55f1a6d12649098f47e57
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-06-25 17:10:33 +0430

    Fixed the linters errors

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ const userInput = document.getElementById('text-input');
 const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
-const checkForPalindrome = input => {
-  const originalInput = input; // Store for later output
+const checkForPalindrome = (input) => {
+  const originalInput = input;
 
   if (input === '') {
     alert('Please input a value');
     return;
   }
 
-  // Remove the previous result
   resultDiv.replaceChildren();
 
   const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
@@ -23,7 +22,6 @@ const checkForPalindrome = input => {
   pTag.innerHTML = resultMsg;
   resultDiv.appendChild(pTag);
 
-  // Show the result.
   resultDiv.classList.remove('hidden');
 };
 
